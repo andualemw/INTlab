66c25c3f89ec942657b8c4101367b14c3b961f7a
davidAvigad <david.avigad@gmail.com>
2016-12-04 01:20:03 +0200

    04dec/0200

--- EX1/d3/js/indexCTRL.js
+++ EX1/d3/js/indexCTRL.js
@@ -36,13 +36,30 @@ myApp.run(function($http){
                         .attr("transform", "translate(8," + height + ")")
                         .call(xAxis);
 
+            var div = d3.select("body").append("div")   
+                        .attr("class", "tooltip")               
+                        .style("opacity", 0);
+                
             var circle = canvas.selectAll("circle")
                         .data(dataSet)
                         .enter()
                         .append("circle")
                         .attr("r", 1)
-                        .attr("cx", function(ds, i){console.log(i);return(x(i));})
+                        .attr("cx", function(ds, i){return(x(i));})
                         .attr("cy", function(ds){return(y(ds.finalGrade));})
+                        .on("mouseover", function(d) {      
+                            div.transition()        
+                                .duration(200)      
+                                .style("opacity", .9);      
+                            div .html("<h6><b>"+d.firstName +" "+ d.lastName  + "</b></h6>" + "finalGrade: " + d.finalGrade)  
+                                .style("left", (d3.event.pageX) + "px")     
+                                .style("top", (d3.event.pageY - 28) + "px");    
+                            })                  
+                        .on("mouseout", function(d) {       
+                            div.transition()        
+                                .duration(500)      
+                                .style("opacity", 0);   
+                        })
                         .attr("fill", function(ds){return(d3.interpolateMagma(Math.random()));})
                         .attr("fill-opacity", function(ds, i){return(ds.finalGrade * 0.01)})
                         .transition()
@@ -51,20 +68,19 @@ myApp.run(function($http){
                         .transition()
                         .duration(1000)
                         .attr("r", 8);
-
-            var text = canvas.selectAll("div")
-                        .data(dataSet)
-                        .enter()
-                        .append("text")
-                        .attr("fill", "blue")
-                        .attr("y", function(ds){return(y(ds.finalGrade)-10);})
-                        .attr("x", function(ds, i){return(x(i)+20);})
-                        .on('mouseover', function(){})
-                        .on('mouseout', this.hide)
-                        .text(function(ds){console.log(ds.firstName);return ds.firstName}); console.log(text);         
+            //Add lines to dots
+            // var line = canvas.selectAll("div")
+            //             .data(dataSet)
+            //             .enter()
+            //             .append("div")
+            //             .class("stroke")
+            //             .attr("x1", function(ds, i){return(x(i)+20);})
+            //             .attr("y1", function(ds){return(y(ds.finalGrade)-10);})
+            //             .attr("x2", function(ds, i){return(x(i)+20);})
+            //             .attr("y2", 0);
         });
         //not in use. for top menu
-        $( document ).tooltip({
-            tooltipClass: "custom-tooltip-styling"
-        });
+        // $( document ).tooltip({
+        //     tooltipClass: "custom-tooltip-styling"
+        // });
 })
